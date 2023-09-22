4ab3d63fef7da7feb3be08536500133fc856ff81
Yuriy Nikolskiy <bankirerbe@gmail.com>
2023-09-23 02:43:00 +0300

    first commit

---
+++ Program_Task8.cs
@@ -0,0 +1,21 @@
+﻿Console.Write("Enter a number: ");
+string number = Console.ReadLine();
+int N = Int32.Parse(number);
+
+int count = 2;
+while(count < N-1)
+{
+    Console.Write(count);        
+    Console.Write(", ");        
+    count = count + 2;
+}
+if(count == N)
+{
+    Console.Write(count);        
+    Console.Write(" ");
+}
+if(count == N-1)
+{
+    Console.Write(count);        
+    Console.Write(" ");
+}
